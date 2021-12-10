83408e1ba2e06b2460584b1165ec4bb973af2dac
Ethan Brooks <ethanabrooks@gmail.com>
2021-12-10 13:01:29 -0500

    take out lambda

--- supervised/antonyms_synonyms.py
+++ supervised/antonyms_synonyms.py
@@ -314,12 +314,7 @@ def train(args: Args, logger: HasuraLogger):
 
     if baseline:
         _, inputs = inputs.unique(return_inverse=True)
-        bag = nn.EmbeddingBag(int(inputs.max() + 1), embedding_size).to(device)
-
-        def f(x):
-            return bag(x)
-
-        encoder = Lambda(f)
+        encoder = nn.EmbeddingBag(int(inputs.max() + 1), embedding_size).to(device)
 
     else:
         encoder = GPTEmbed(
