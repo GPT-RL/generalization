1a6905f31748ce30ddfd01e2826f21a5dbc080f4
Ethan Brooks <ethanabrooks@gmail.com>
2022-01-23 10:44:00 -0500

    use Linear to initialize keys instead of Embedding

--- rl/gpt_agent.py
+++ rl/gpt_agent.py
@@ -24,7 +24,7 @@ class Base(babyai_agent.Base):
         self.train_wpe = train_wpe
         self.train_ln = train_ln
         super().__init__(*args, pretrained_model=pretrained_model, **kwargs)
-        self.keys = nn.Embedding(num_embeddings, self.embedding_size)
+        self.keys = nn.Linear(num_embeddings, self.embedding_size)
         self.values = nn.Embedding(num_embeddings, self.embedding_size)
         self.multihead_attn = nn.MultiheadAttention(self.embedding_size, num_heads=1)
 
@@ -41,7 +41,7 @@ class Base(babyai_agent.Base):
         states = self.embeddings.forward(
             inputs, attention_mask=inputs != self.pad_token_id
         ).last_hidden_state[:, -2:]
-        key = self.keys.weight.unsqueeze(1)
+        key = self.keys.weight.T.unsqueeze(1)
         value = self.values.weight.unsqueeze(1)
         attn_output, _ = self.multihead_attn.forward(query=states, key=key, value=value)
         return attn_output.mean(1)
