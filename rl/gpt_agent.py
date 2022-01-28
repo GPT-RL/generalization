0eb06d05486ba48310d3cd2ace18f169cde0592c
Ethan Brooks <ethanabrooks@gmail.com>
2022-01-28 08:49:50 -0500

    add HashTensorWrapper/lru_cache optimization for not (train_ln or train_wpe)

--- rl/gpt_agent.py
+++ rl/gpt_agent.py
@@ -1,10 +1,52 @@
+import itertools
+from dataclasses import dataclass
+from functools import lru_cache
+from typing import cast
+
 import babyai_agent
+import torch
+from torch import Tensor
 from utils import build_gpt
 
 
 class Agent(babyai_agent.Agent):
     def build_base(self, obs_shape, **kwargs):
         return Base(**kwargs)
+
+
+def get_primes():
+    for i in itertools.count():
+        if not any(i % j == 0 for j in range(2, i // 2 + 1)):
+            yield i
+
+
+@lru_cache()
+def get_primes_tensor(num_el, device, shape):
+    return torch.tensor(
+        list(itertools.islice(get_primes(), num_el)),
+        device=device,
+    ).reshape(shape)
+
+
+@dataclass(frozen=True)
+class HashTensorWrapper:
+    """
+    https://discuss.pytorch.org/t/how-to-put-tensors-in-a-set/123836/7
+    """
+
+    tensor: Tensor
+
+    def __hash__(self):
+        primes = get_primes_tensor(
+            self.tensor.numel(), self.tensor.device, self.tensor.shape
+        )
+        return torch.sum(self.tensor * primes).item()
+
+    def __eq__(self, other):
+        assert isinstance(other, HashTensorWrapper)
+        equals = self.tensor == other.tensor
+        equals = cast(Tensor, equals)
+        return torch.all(equals)
 
 
 class Base(babyai_agent.Base):
@@ -32,7 +74,24 @@ class Base(babyai_agent.Base):
             p.requires_grad_(requires_grad)
         return gpt
 
+    @staticmethod
+    def process_last_hidden_state(last_hidden_state: Tensor):
+        return last_hidden_state[:, -1]
+
+    @lru_cache()
+    def _embed(self, inputs: HashTensorWrapper):
+        with torch.no_grad():
+            forward = self.embeddings.forward(
+                inputs.tensor, attention_mask=inputs.tensor != self.pad_token_id
+            )
+        return self.process_last_hidden_state(forward.last_hidden_state)
+
     def embed(self, inputs):
-        return self.embeddings.forward(
-            inputs, attention_mask=inputs != self.pad_token_id
-        ).last_hidden_state[:, -1]
+        if self.train_ln or self.train_wpe:
+            state = self.embeddings.forward(
+                inputs, attention_mask=inputs != self.pad_token_id
+            ).last_hidden_state
+            return self.process_last_hidden_state(state)
+        else:
+            embedded = [self._embed(HashTensorWrapper(x)) for x in inputs.unsqueeze(1)]
+            return torch.cat(embedded, dim=0)
