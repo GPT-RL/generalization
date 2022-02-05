ce9cca66341dc35bd1d690f52c74fe8392e11701
Ethan Brooks <ethanabrooks@gmail.com>
2022-02-05 18:00:40 -0500

    got things working with headless_device

--- my_ray.py
+++ my_ray.py
@@ -3,10 +3,23 @@ import ray
 ray.init()
 
 
-@ray.remote
-def f(x):
-    return x * x
+# noinspection PyCallingNonCallable,PyArgumentList
+@ray.remote(num_gpus=1)
+def f():
+    import pyglet
+
+    print(ray.get_gpu_ids())
+
+    pyglet.options["headless"] = True
+    from gym_miniworld.envs import OneRoom
+
+    env = OneRoom()
+    while True:
+        env.reset()
+        t = False
+        while not t:
+            s, r, t, i = env.step(env.action_space.sample())
 
 
-futures = [f.remote(i) for i in range(4)]
+futures = [f.remote() for i in range(4)]
 print(ray.get(futures))  # [0, 1, 4, 9]
