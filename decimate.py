10955b6d3271320135b71a8cbeb2078840e14244
Ethan Brooks <ethanabrooks@gmail.com>
2022-02-09 17:05:56 -0500

    update decimate.py to downsample PNGs

--- decimate.py
+++ decimate.py
@@ -1,7 +1,7 @@
-import shutil
 from pathlib import Path
 
 import pymeshlab
+from PIL import Image
 from tap import Tap
 from tqdm import tqdm
 
@@ -27,10 +27,24 @@ def main(args: Args):
         output_path = input_obj.relative_to(input_dir)
         output_path = Path(output_dir, output_path)
         output_path.parent.mkdir(exist_ok=True, parents=True)
-        for path in input_obj.parent.iterdir():
-            if path.name != output_path.name and path.suffix in ("mtl", "obj", "png"):
-                shutil.copy2(path, output_path.with_name(path.name))
         ms.save_current_mesh(str(output_path))
+        for path in output_path.parent.glob("*.png"):
+            # from https://stackoverflow.com/a/273962
+            img: Image = Image.open(path)
+            w, h = [round(d * args.percent) for d in img.size]
+
+            if "P" in img.mode:  # check if image is a palette type
+                img = img.convert("RGB")  # convert it to RGB
+                img = img.resize((w, h), Image.ANTIALIAS)  # resize it
+                img = img.convert("P", dither=Image.NONE, palette=Image.ADAPTIVE)
+                # convert back to palette
+            else:
+                img = img.resize((w, h), Image.ANTIALIAS)  # regular resize
+            img.save(
+                output_path.with_name(path.name)
+            )  # save the image to the new source
+            # img.save(newSourceFile, quality = 95, dpi=(72,72), optimize = True)
+            # set quality, dpi , and shrink size
 
 
 if __name__ == "__main__":
