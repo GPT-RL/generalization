060e21f89822655c07112eaee9cf932a9e3c28e2
Ethan Brooks <ethanabrooks@gmail.com>
2022-02-10 10:15:21 -0500

    use timestep,rank as the key

--- rl/my/env.py
+++ rl/my/env.py
@@ -183,7 +183,7 @@ class Env(MiniWorldEnv):
             if done:
                 info.update(pair=(self._mission, self._dist_name))
 
-            R.set(str(self.timestep), pickle.dumps(obs))
+            R.set(f"{self.timestep},{self.rank}", pickle.dumps(obs))
             self.timestep += 1
             action = yield obs, reward, done, info
             action = cast(MiniWorldEnv.Actions, action)
