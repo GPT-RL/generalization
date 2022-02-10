8ce369944cafc2f6e704c8a3dc1702500ea05110
Ethan Brooks <ethanabrooks@gmail.com>
2022-02-10 11:17:26 -0500

    add --net host

--- run.sh
+++ run.sh
@@ -4,6 +4,7 @@ name=$(basename "$PWD")_agent
 docker build -t "$name" .
 docker run --rm -it \
 	--env-file .env \
+	--net host \
 	--gpus "$1" \
 	-e HOST_MACHINE="$(hostname -s)" \
 	-e TERM=xterm-256color \
