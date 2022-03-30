40c81440217ebf553e19f19e1d081756a00a5f07
Ethan Brooks <ethanabrooks@gmail.com>
2022-03-30 11:26:32 -0400

    delete gpt_main

--- rl/main.py
+++ rl/main.py
@@ -14,8 +14,7 @@ try:
 except ValueError:
     device = None
 pyglet.options["headless_device"] = device
-from base_main import ArgsType  # noqa: E402
-from gpt_main import Args, Trainer  # noqa: E402
+from my.main import Args, ArgsType, Trainer  # noqa: E402
 
 if __name__ == "__main__":
     Trainer.main(cast(ArgsType, Args().parse_args()))
